dd4b642d126bca7aa498a97ebd5d88cf1944371a
Errissonds <errissonds16@hotmail.com>
2024-10-08 15:31:41 -0300

    alteração em todos os niveis

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log ("ola mundao")
+// Desafio classificador nivel de heroi
+const frase = ("O Herói de nome ERRISSON está no nível de ")
+
+let XP = 10001
+if (XP <= 1000) {
+console.log (frase + "Ferro!")
+} else if (XP >= 1001 , XP <= 2000) 
+    {
+  console.log (frase + "Bronze!")
+} else if (XP >= 2001 , XP <= 5000) 
+    {
+  console.log (frase + "Prata!")
+} else if (XP >= 5001 , XP <= 7000) 
+    {
+  console.log (frase + "Ouro!")
+} else if (XP >= 7001 , XP <= 8000) 
+    {
+  console.log (frase + "Platina!")
+} else if (XP >= 8001 , XP <= 9000) 
+    {
+  console.log (frase + "Ascendente!")
+} else if (XP >= 9001 , XP <= 10000) 
+    {
+  console.log (frase + "Imortal!")
+} else {
+  console.log (frase + "Radiante!")
+}
