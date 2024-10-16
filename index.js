b5306500299f9e378007e864e5433b9b2c2277e2
Errissonds <errissonds16@hotmail.com>
2024-10-16 01:43:00 -0300

    alterando frase de saida

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 // Desafio classificador nivel de heroi
 const frase = ("O Herói de nome ERRISSON está no nível de ")
+let nome = ("Errisson")
+let XP = 15000
 
-let XP = 10001
 if (XP <= 1000) {
-console.log (frase + "Ferro!")
+console.log ( "O Herói " + nome + " está no nível de Ferro!")
 } else if (XP >= 1001 , XP <= 2000) 
     {
-  console.log (frase + "Bronze!")
+  console.log ("O Herói " + nome + " está no nível de Bronze!")
 } else if (XP >= 2001 , XP <= 5000) 
     {
-  console.log (frase + "Prata!")
+  console.log ("O Herói " + nome + " está no nível de Prata!")
 } else if (XP >= 5001 , XP <= 7000) 
     {
-  console.log (frase + "Ouro!")
+  console.log ("O Herói " + nome + " está no nível de Ouro!")
 } else if (XP >= 7001 , XP <= 8000) 
     {
-  console.log (frase + "Platina!")
+  console.log ("O Herói " + nome + " está no nível de Platina!")
 } else if (XP >= 8001 , XP <= 9000) 
     {
-  console.log (frase + "Ascendente!")
+  console.log ("O Herói " + nome + " está no nível de Ascendente!")
 } else if (XP >= 9001 , XP <= 10000) 
     {
-  console.log (frase + "Imortal!")
+  console.log ("O Herói " + nome + " está no nível de Imortal!")
 } else {
-  console.log (frase + "Radiante!")
+  console.log ("O Herói " + nome + " está no nível de Radiante!")
 }
